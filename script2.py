3b912443cdbf274f97cd2175ca34c8d0ee0be0f3
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-04-02 16:41:29 +0530

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,13 +1,13 @@
 from machine import Pin
 from neopixel import NeoPixel
 from time import sleep
-
+np = NeoPixel(Pin(5), 60)
 while True:
      for i in range(60):
       np[i]=(10,0,20)
       np.write()
       sleep(0.05)
-      for i in range(60):
+     for i in range(60):
       np[i]=(0,0,0)
       np.write()
       sleep(0.05)
